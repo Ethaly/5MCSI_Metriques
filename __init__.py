5605b8f94fc7f11b6daeca654aa296b555882a7a
Ethaly <ethaly1@myges.fr>
2024-11-21 16:50:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,17 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
+    
 @app.route("/contact/")
 def MaPremiereAPI():
     return render_template("forms.html")
+    
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -22,12 +26,14 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+    
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+    
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
+    
 if __name__ == "__main__":
   app.run(debug=True)
-Dispose d’un menu contextuel
